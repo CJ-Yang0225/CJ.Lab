48536978e95b08a414941dc627531821fcb27875
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-08-19 22:33:36 +0800

    feat: add shiny effect button with pseudo-element

---
+++ components/Button/styled/ShinyButton.ts
@@ -0,0 +1,55 @@
+import { keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import { move } from "../../animations";
+import Button from "../Button";
+
+const sheen = keyframes`
+  100% {
+    transform: rotateZ(60deg) translate(1em, -9em);
+  }
+`;
+
+const ShinyButton = styled(Button)`
+  position: relative;
+  overflow: hidden;
+
+  padding: 0.5em 1em;
+
+  border: none;
+  border-radius: 0.5em;
+  ${({ backgroundColor, textColor }) => ({
+    color: textColor,
+    backgroundColor,
+  })};
+  font-size: 20px;
+  line-height: 32px;
+  transition: all 0.5s cubic-bezier(0.67, -0.51, 0.42, 1.43);
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: -50%;
+    right: -50%;
+    bottom: -50%;
+    left: -50%;
+    background: linear-gradient(
+      to bottom,
+      transparent,
+      hsla(0, 0%, 100%, 0.5) 50%,
+      transparent
+    );
+    transform: rotateZ(60deg) translate(0, 7.5em);
+  }
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.25);
+  }
+
+  &:hover::after,
+  &:focus::after {
+    animation: ${sheen} 1s forwards;
+  }
+`;
+
+export default ShinyButton;
---
+++ pages/works/buttons/shiny.tsx
@@ -0,0 +1,25 @@
+import ShinyButton from "../../../components/Button/styled/ShinyButton";
+
+function Shiny() {
+  return (
+    // maybe need a card component
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        borderRadius: "1em",
+        padding: "40px 48px",
+        width: "clamp(288px, 25%, 360px)",
+        backgroundColor: "#f1f3f4",
+      }}
+    >
+      <ShinyButton
+        backgroundColor="#d1703c"
+        textColor="#ffffff"
+        children="SHINY EFFECT"
+      />
+    </div>
+  );
+}
+
+export default Shiny;
