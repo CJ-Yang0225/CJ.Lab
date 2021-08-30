6d9ec3b0b5902344e52da629d1df124446b7a469
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-08-30 23:30:10 +0800

    chore: add glsl file loaders

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const plugins = require("next-compose-plugins"); // for glsl files
+
+const nextConfig = {
+  webpack(config, { webpack, dev, isServer }) {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ["raw-loader", "glslify-loader"],
+    });
+
+    return config;
+  },
+};
+
+module.exports = plugins([], nextConfig);
