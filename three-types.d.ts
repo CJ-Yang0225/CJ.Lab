cc23c4ab2f2e89a81eeab717f681b56e48e1aa29
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-08-30 23:34:10 +0800

    chore: add type for test shaders & modify tsconfig

--- three-types.d.ts
+++ three-types.d.ts
@@ -1,1 +1,12 @@
-export * from "@react-three/fiber/dist/react-three-fiber.cjs";
+import { ReactThreeFiber } from "@react-three/fiber/dist/react-three-fiber.cjs";
+import { SphereGeometry } from "three";
+
+// export * from "@react-three/fiber/dist/react-three-fiber.cjs";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      haloTest: ReactThreeFiber.Object3DNode<HaloTest, typeof HaloTest>;
+    }
+  }
+}
