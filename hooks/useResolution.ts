ff80fc47f1d676938fe38fa313e77d0bdecf5aae
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-08-31 23:51:10 +0800

    feat: change to isomorphic version

--- hooks/useResolution.ts
+++ hooks/useResolution.ts
@@ -1,22 +1,24 @@
 import { useEffect, useLayoutEffect, useMemo, useState } from "react";
-import { debounce, throttle } from "../lib/utils";
+import { debounce } from "../lib/utils";
 
-type Resolution = [number, number];
+const useIsomorphicLayoutEffect =
+  typeof window === "undefined" ? useEffect : useLayoutEffect;
+
+export type Resolution = [number, number];
 
 export const useResolution = (
   options = { savingMode: false, immediate: false }
 ) => {
   const { savingMode, immediate } = useMemo(() => options, [options]);
   const [resolution, setResolution] = useState<Resolution>([0, 0]); // [window.innerWidth, window.innerHeight] causes 'window is not defined error'
+  const updateResolution = () => {
+    setResolution([window.innerWidth, window.innerHeight]);
+  };
 
-  useLayoutEffect(() => {
-    const updateResolution = () => {
-      setResolution([window.innerWidth, window.innerHeight]);
-    };
-
+  useIsomorphicLayoutEffect(() => {
     updateResolution();
 
-    const debouncedUpdateResolution = debounce(updateResolution, 50, immediate);
+    const debouncedUpdateResolution = debounce(updateResolution, 100, immediate);
 
     savingMode
       ? window.addEventListener("resize", debouncedUpdateResolution)
