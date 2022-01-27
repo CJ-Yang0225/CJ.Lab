1163ff3207bffcbabaa6c61ba2b01daf59dc8de3
ChaoJie Yang <cj.yang0225@gmail.com>
2022-01-28 02:43:03 +0800

    feat(canvas): opimize & add mobile compatibility

--- components/common/Canvas/particles/rgb.ts
+++ components/common/Canvas/particles/rgb.ts
@@ -24,16 +24,16 @@ class Particle {
     this.ctx = ctx;
     this.x = mouse.x;
     this.y = mouse.y;
-    this.size = Math.random() * 15 + 1;
-    this.speedX = Math.random() * 3 - 1.5;
-    this.speedY = Math.random() * 3 - 1.5;
+    this.size = Math.random() * 20 + 1;
+    this.speedX = Math.random() * 4 - 2;
+    this.speedY = Math.random() * 4 - 2;
     this.color = `hsl(${hue}, 100%, 50%)`; // To keep color
   }
 
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
-    if (this.size > 0.2) this.size -= 0.1;
+    if (this.size > 0.3) this.size -= 0.1;
   }
 
   draw() {
@@ -70,10 +70,10 @@ export function renderParticles(canvas: HTMLCanvasElement) {
           const dy = particles[i].y - particles[j].y;
           const distance = Math.sqrt(dx ** 2 + dy ** 2);
 
-          if (distance > 105 && distance < 120) {
+          if (distance > 100 && distance < 120) {
             ctx.beginPath();
             ctx.strokeStyle = particles[i].color;
-            ctx.lineWidth = 0.3;
+            ctx.lineWidth = 0.5;
             ctx.moveTo(particles[i].x, particles[i].y);
             ctx.lineTo(particles[j].x, particles[j].y);
             ctx.stroke();
@@ -97,34 +97,47 @@ export function renderParticles(canvas: HTMLCanvasElement) {
         break;
 
       case Mode.fadeOut:
-        ctx.fillStyle = `rgba(0, 0, 0, 0.035)`;
+        ctx.fillStyle = `rgba(0, 0, 0, 0.03)`;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         break;
     }
 
     handleParticles();
     hue += 3;
+    hue %= 360;
     animationFrameId = requestAnimationFrame(render);
   };
 
   render();
 
+  const handleModeSwitch = () => {
+    mode++;
+    mode %= 2;
+  };
+
   const handleMouseMove = throttle((event: MouseEvent) => {
     mouse.x = event.clientX;
     mouse.y = event.clientY;
     fillParticles();
-  }, 20);
+  }, 25);
 
-  const handleClick = () => {
-    mode++;
-    mode %= 2;
-  };
-  canvas.addEventListener("click", handleClick);
+  const handleTouchMove = throttle((event: TouchEvent) => {
+    const touch = event.touches[0];
+    const mouseEvent = new MouseEvent("mousemove", {
+      clientX: touch.clientX,
+      clientY: touch.clientY,
+    });
+    canvas.dispatchEvent(mouseEvent);
+  }, 30);
+
+  canvas.addEventListener("click", handleModeSwitch);
   canvas.addEventListener("mousemove", handleMouseMove);
+  canvas.addEventListener("touchmove", handleTouchMove);
 
   return () => {
     cancelAnimationFrame(animationFrameId);
-    removeEventListener("click", handleClick);
+    removeEventListener("click", handleModeSwitch);
     removeEventListener("mousemove", handleMouseMove);
+    removeEventListener("touchmove", handleTouchMove);
   };
 }
