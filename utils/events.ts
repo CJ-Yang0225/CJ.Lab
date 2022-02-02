25a166f027559522d68937a235a05e1e67c97972
ChaoJie Yang <cj.yang0225@gmail.com>
2022-02-02 20:34:41 +0800

    feat(utils): add EventStore to manage event listeners

---
+++ utils/events.ts
@@ -0,0 +1,20 @@
+export class EventStore {
+  private _listeners: (() => void)[];
+
+  add(
+    target: EventTarget,
+    type: Event["type"],
+    listener: EventListener,
+    options: AddEventListenerOptions = { passive: true }
+  ) {
+    target.addEventListener(type, listener, options);
+    this._listeners.push(
+      removeEventListener.bind(target, type, listener, options)
+    );
+  }
+
+  clean() {
+    this._listeners.forEach((remove) => remove());
+    this._listeners.length = 0;
+  }
+}
