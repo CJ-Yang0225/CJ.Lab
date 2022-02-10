3a4cc1da80dabedea8e6c03904fb45498727a6a5
ChaoJie Yang <cj.yang0225@gmail.com>
2022-02-10 14:19:22 +0800

    feat(3d): add the first version of grass

--- 3d/scenes/Minecraft/Grass.tsx
+++ 3d/scenes/Minecraft/Grass.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import { PlaneProps, usePlane } from "@react-three/cannon";
 
-function Grass() {
+function Grass(props: PlaneProps) {
+  const [planeRef] = usePlane(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    ...props,
+  }));
 
   return (
-    <React.Fragment>
-
-    </React.Fragment>
-  )
+    <mesh ref={planeRef} receiveShadow>
+      <planeGeometry args={[1000, 1000]} />
+      {/* <meshStandardMaterial color="green" /> */}
+      <meshPhongMaterial color={"#5b8b32"} />
+    </mesh>
+  );
 }
 
 export default Grass;
