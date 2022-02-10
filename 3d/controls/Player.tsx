7d9ba9d780b8ea63adcfb2be83e50a573854d68a
ChaoJie Yang <cj.yang0225@gmail.com>
2022-02-10 14:54:47 +0800

    feat(3d): add new props and use hook

--- 3d/controls/Player.tsx
+++ 3d/controls/Player.tsx
@@ -1,10 +1,12 @@
 import { PerspectiveCamera, PointerLockControls } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
+import { usePlayerControls } from "../hooks/usePlayerControls";
 
 import { playerState } from "../three.config";
 
 export type PlayerProps = {
-  isCustomCamera?: boolean;
+  activateCustomControls?: boolean;
+  perspective?: 1 | 2 | 3;
   speed?: number;
   position?: [x: number, y: number, z: number];
   rotation?: [x: number, y: number, z: number, w: number];
@@ -12,15 +14,17 @@ export type PlayerProps = {
 
 function Player(props: PlayerProps) {
   const {
-    isCustomCamera = true,
+    activateCustomControls = true,
+    perspective = 1, // TODO: switch perspective: first person & third person
     speed = playerState.SPEED, // TODO: calculate physical effects
-    position = [0, 0, 0],
+    position = [0, 1, 0],
     rotation: quaternion = [0, 0, 0, 1],
   } = props;
 
   const camera = useThree((state) => state.camera);
+  usePlayerControls()
 
-  if (isCustomCamera) {
+  if (activateCustomControls) {
     camera.position.set(...position); // TODO: initial value of gravity and physics
     camera.quaternion.set(...quaternion);
 
