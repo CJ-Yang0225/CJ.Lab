49212743ec3e9fd38b3e4f8364cd2b76f9dbc351
ChaoJie Yang <cj.yang0225@gmail.com>
2022-02-15 20:59:10 +0800

    feat(3d/hooks): add basic camera position control

--- 3d/hooks/usePlayerControls.ts
+++ 3d/hooks/usePlayerControls.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { useFrame, useThree } from "@react-three/fiber";
+import { useFrame, useThree, Vector3, Vector4 } from "@react-three/fiber";
 import { useKeyboard } from "../../hooks/useKeyboard";
 
 const movementByKey: Record<string, string> = {
@@ -23,9 +23,16 @@ const sideVector = new THREE.Vector3();
 const rotation = new THREE.Vector3();
 const speed = new THREE.Vector3();
 
-export function usePlayerControls() {
+export type PlayerControlsProps = {
+  speed: number;
+  position: Vector3;
+  rotation: Vector4;
+};
+
+export function usePlayerControls(props: PlayerControlsProps) {
+  const { speed, position, rotation } = props;
   const movement = useRef({ ...initialMovement });
-  const camera = useThree((state) => state.camera);
+  const { camera, scene } = useThree();
   const pressedKeys = useKeyboard();
   const velocity = useRef([0, 0, 0]).current; // physicals
 
@@ -42,13 +49,24 @@ export function usePlayerControls() {
     );
 
     Object.assign(movement.current, pressedKeysMap);
-    console.log("movement: ", movement.current);
+    // console.log("movement: ", movement.current);
   }, [movement, pressedKeys]);
 
   useFrame(() => {
     const { forward, backward, left, right, jump } = movement.current;
     frontVector.set(0, 0, Number(backward) - Number(forward));
     sideVector.set(Number(right) - Number(left), 0, 0);
-    direction.addVectors(frontVector, sideVector).normalize().multiplyScalar;
+    const { x: px, y: py, z: pz } = camera.position;
+    const { x: rx, y: ry, z: rz } = camera.rotation;
+    const r = new THREE.Euler(rx, ry, rz, "XYZ");
+    direction
+      .addVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(speed)
+      .applyEuler(r);
+    console.log("direction: ", direction);
+    camera.position.set(px + direction.x, py, pz + direction.z);
   });
+
+  return;
 }
