83f396d64fcc3e4ea0d48e67028558368be17afa
ChaoJie Yang <cj.yang0225@gmail.com>
2022-02-15 21:27:00 +0800

    feat(3d/scenes): separate camera and player

--- 3d/scenes/Minecraft/index.tsx
+++ 3d/scenes/Minecraft/index.tsx
@@ -3,7 +3,8 @@ import { Physics } from "@react-three/cannon";
 import { Sky } from "@react-three/drei";
 
 import Grass from "./Grass";
-import Player from "../../controls/Player";
+import Controls from "../../controls/Controls";
+import Player from "../../entities/Player";
 
 type WorldProps = {
   ecosystem: string;
@@ -12,13 +13,14 @@ type WorldProps = {
 function World(props: WorldProps) {
   return (
     <React.Suspense fallback={null}>
-      <Sky sunPosition={[100, 20, 100]} />
+      <Sky sunPosition={[100, 20, 100]} distance={1000} />
       <ambientLight intensity={0.3} />
       <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
       <Physics>
         <Grass />
-        <Player activateCustomControls={false} />
+        <Player />
       </Physics>
+      <Controls activateCustomControls={false} />
     </React.Suspense>
   );
 }
