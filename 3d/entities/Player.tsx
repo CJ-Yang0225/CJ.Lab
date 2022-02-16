140c2f8503114ce6d7c20e2598eb3ef6e29f3036
ChaoJie Yang <cj.yang0225@gmail.com>
2022-02-16 16:05:48 +0800

    feat(3d): add controls hook

--- 3d/entities/Player.tsx
+++ 3d/entities/Player.tsx
@@ -2,20 +2,18 @@ import { usePlayerControls } from "../hooks/usePlayerControls";
 import { playerState } from "../three.config";
 
 export type PlayerProps = {
-  perspective?: 1 | 2 | 3;
   speed?: number;
-  position?: [x: number, y: number, z: number];
-  rotation?: [x: number, y: number, z: number, w: number];
+  position?: Three.Object3D["position"];
+  quaternion?: Three.Object3D["quaternion"];
 };
 
 function Player(props: PlayerProps) {
   const {
-    perspective = 1, // TODO: switch perspective: first person & third person
-    speed = playerState.SPEED, // TODO: calculate physical effects
+    speed = playerState.SPEED,
     position = [0, playerState.HEIGHT, 0],
-    rotation = [0, 0, 0, 1],
+    quaternion = [0, 0, 0, 1],
   } = props;
-  // const ref = usePlayerControls({ speed, position, rotation });
+  const ref = usePlayerControls({ speed, position, quaternion });
   return <mesh></mesh>;
 }
 
