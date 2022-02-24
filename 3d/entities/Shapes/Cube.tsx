720037455c7d7868466d1231bff15a6d1b78dfc7
ChaoJie Yang <cj.yang0225@gmail.com>
2022-02-24 09:50:12 +0800

    feat(3d): add basic Cube shaped

--- 3d/entities/Shapes/Cube.tsx
+++ 3d/entities/Shapes/Cube.tsx
@@ -1,5 +1,39 @@
-import { BoxProps } from "@react-three/cannon";
+import { useState } from "react";
+import { BoxProps, useBox } from "@react-three/cannon";
+import { Color } from "@react-three/fiber";
 
-function Cube(props: BoxProps) {}
+type CubeProps = BoxProps & {
+  color?: Color;
+  layers?: number;
+};
+
+const randomColor = () => {
+  const r = Math.ceil(Math.random() * 255).toString(16);
+  const g = Math.ceil(Math.random() * 255).toString(16);
+  const b = Math.ceil(Math.random() * 255).toString(16);
+
+  return `#${r}${g}${b}`;
+};
+
+function Cube(props: CubeProps) {
+  const { color, layers } = props;
+  const [materialColor] = useState(color || randomColor);
+  const [cubeRef, api] = useBox(() => ({
+    mass: 30,
+    args: [0.5, 0.5, 0.5],
+    material: {
+      friction: 1,
+      restitution: 0,
+    },
+    ...props,
+  }));
+  console.log("color:", materialColor);
+  return (
+    <mesh ref={cubeRef} castShadow layers={layers}>
+      <boxBufferGeometry args={[0.5, 0.5, 0.5]} />
+      <meshLambertMaterial color={materialColor} />
+    </mesh>
+  );
+}
 
 export default Cube;
