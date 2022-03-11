10884defa67a45e9362c0460bb8f53f5ec8c25ac
ChaoJie Yang <cj.yang0225@gmail.com>
2022-03-12 00:05:43 +0800

    chore: change file extension & rm unused code

--- components/layout/Gallery/Flex.tsx
+++ components/layout/Gallery/Flex.tsx
@@ -5,8 +5,6 @@ export interface FlexProps extends IntrinsicElementProps<"div"> {}
 function Flex(props: FlexProps) {
   const { children } = props;
 
-  console.log("styles:", styles);
-
   return <div className={styles["flex-box"]}>{children}</div>;
 }
 
